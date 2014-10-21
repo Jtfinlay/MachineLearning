00669d5d3da59462021cfc2df7678da4cfb69f92
jtfinlay <jtfinlay@ualberta.ca>
2014-10-21 11:03:44 -0600

    One vs All working.

--- oneVsAll.m
+++ oneVsAll.m
@@ -7,17 +7,23 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %   in a matrix all_theta, where the i-th row of all_theta corresponds 
 %   to the classifier for label i
 
-% Some useful variables
 m = size(X, 1);
 n = size(X, 2);
 
-% You need to return the following variables correctly 
 all_theta = zeros(num_labels, n + 1);
 
-% Add ones to the X data matrix
 X = [ones(m, 1) X];
 
 % ====================== YOUR CODE HERE ======================
+initial_theta = zeros(n + 1, 1);
+
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+
+for c = 1:num_labels
+    all_theta(c,:) = fmincg(@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+end
+
+
 % Instructions: You should complete the following code to train num_labels
 %               logistic regression classifiers with regularization
 %               parameter lambda. 
@@ -48,19 +54,6 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
-
-
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
 
 
 end
