3013790ec2df50a032192cd3c988cb6f9e617e7f
James Finlay <jtfinlay@ualberta.ca>
2014-11-25 15:28:10 -0700

    Import original files

---
+++ 6/PE6.m
@@ -0,0 +1,49 @@
+%% CMPUT 466/551 (2014)
+%% PE#6 script
+
+%% HMM State transition matrix
+A = [0.80, 0.20; 0.1 0.9];
+
+%% HMM Emission Matrix
+B = [1/6 4/5; 1/6 1/25; 1/6 1/25; 1/6 1/25; 1/6 1/25; 1/6 1/25];
+
+%% Observations from HMM
+O = [4, 1, 2, 3, 1, 3, 1, 1, 5, 6];
+
+%% initial state distribution
+%This is P(D0)
+phi_0 = [0.5 0.5];
+
+%Using the initial state distribution, predict the state distribution
+%before evidence i.e. P(D1)
+%You code here for (a)
+
+fprintf('(a) P(D_1)')
+
+%phi=
+
+%% Uncomment this portion to use appropriate variables  
+
+%[alpha, P_O] = forward(O, phi, A, B);
+
+%beta = backward(O, A, B);
+
+%[dstar, pstar] = viterbi(O, phi, A, B);
+
+%% Now answer the Questions:
+
+% (b): P(D_t = r | S_1:t)
+fprintf('(b): P(D_t = r | S_1:t)')
+% alpha is needed here
+
+% (c): P(D_t = r | S_1:10)
+fprintf('(c): P(D_t = r | S_1:10)')
+% gamma is needed here (See Eqn 27 in Rabiner 1989 for details)
+
+% (d): argmax_d P(D=d | S_1:10)
+fprintf('(d): argmax_d P(D=d | S_1:10)\n')
+% use viterbi algorithm
+
+% (e): P_+L(S_1:10) and P_-L(S_1:10)
+fprintf('(e): P_+L(S_1:10) and P_-L(S_1:10)')
+A_L = [0.75 0.25; 0.05 0.95];
---
+++ 6/forward.m
@@ -0,0 +1,26 @@
+function [alpha, P] = forward(O, phi, A, B)
+% Given size-T observation, O, and HMM parameters corresponding to k states,
+% forward.m computes and returns forward matrix, alpha, of size T*k, where
+% alpha(t,i) = Prob(O_{1},O_{2},....O_{t}, D_t=i) and P, of size 1*T, where
+% P(t)=Prob(O_{1},O_{2},....O_{t})
+%
+% Input:
+%   O: sequence of Observations (1*T)
+%   phi: initial state distribution of HMM (1*k)
+%   A: HMM transition matrix (k*k)
+%   B: HMM emission matrix (m*k)
+%
+% Returns:
+%   alpha: forward matrix (T*k)
+%   P: probability of the observation sequence (1*T)
+%
+%   See Eqn 18 in Rabiner 1989 for details
+T = length(O); % size of observation sequence
+m= size(B,1);  % number of possible observed values
+k = size(A,1);  % number of possible states
+alpha = zeros(T, k);
+
+
+%Your code goes here
+
+end
